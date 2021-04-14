f069a5f4707fef9af82cd21c57b2eaef5543ec2d
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-04-14 14:00:56 -0300

    Shown message according to time

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ const express = require('express')
 
 const app = express()
 
+getGreeting = (hour) => {
+    if (hour < 0 && hour >= 24) {
+        return 'Hora inválida'
+    }
+
+    else if (hour >= 6 && hour <= 12) {
+        return 'Buenos dias!'
+    }
+
+    else if (hour >= 13 || hour <= 19) {
+        return 'Buenas tardes!'
+    }
+
+    else {
+        return 'Buenas noches!'
+    }
+}
+
+app.get('/', (req, res) => {
+    let actualHour = new Date().getHours()
+    res.send(getGreeting(actualHour))
+})
+
 const PORT = 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor express escuchando en el puerto ${server.address().port}`)
