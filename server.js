bd0a82254a5b096ab084843de6b57f92feee63c0
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-04-15 23:47:03 -0300

    Done info functionality, reading and writing with file system

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 const { getGreeting, getRandomNumbersObject } = require('./utils')
 
 const app = express()
@@ -16,6 +17,30 @@ app.get('/random', (req, res) => {
     }
     
     res.send(getRandomNumbersObject(numbersQuantity, numbersRange))
+})
+
+app.get('/info', async (req, res) => {
+    const filePackage = './package.json'
+    const fileInfo = './info.txt'
+
+    try {
+        let fileBuffer = await fs.promises.readFile(filePackage, 'utf-8')
+        
+        let fileStat = await fs.promises.stat(filePackage)
+        let info = {
+            contenidoStr: fileBuffer,
+            contenidoObj: JSON.parse(fileBuffer),
+            size: fileStat.size
+        }
+
+        console.log(info)
+        res.send(info)
+
+        let fileToWrite = await fs.promises.writeFile(fileInfo, JSON.stringify(info, null, 2), 'utf-8')
+    }
+    catch(error) {
+        res.send(`<h2 style="color: red;">ERROR 404: recurso no encontrado</h2>`)
+    }
 })
 
 const PORT = 8080
